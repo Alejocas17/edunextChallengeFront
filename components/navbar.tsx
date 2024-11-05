89711bfc177a04d0a8a28c6c8a3fef28f31dec86
Alejandro Castillo <castillogil.10@nlsa.teleperformance.com>
2024-11-04 22:17:37 -0500

    setting githubpages support

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -50,7 +50,7 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <img src="logo.webp" alt="" className="h-10" />
+            <img src="/edunextChallengeFront/logo.webp" alt="" className="h-10" />
             <p className="font-bold text-inherit">EduNext Challenge</p>
           </NextLink>
         </NavbarBrand>
