fbb7d075ced990b47d845d6d64bdb8720b1844be
Alejandro Castillo <castillogil.10@nlsa.teleperformance.com>
2024-11-05 11:05:58 -0500

    fix: fixed responsive navbar issue

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -50,12 +50,12 @@ export const Navbar = () => {
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
+          <NextLink className="flex justify-start items-center gap-1 " href="/">
             <img src="/edunextChallengeFront/logo.webp" alt="" className="h-10" />
-            <p className="font-bold text-inherit">EduNext Challenge</p>
+            <p className="font-bold text-inherit hidden sm:flex">EduNext Challenge</p>
           </NextLink>
         </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+        <ul className=" lg:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <NextLink
